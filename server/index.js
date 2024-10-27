2e2003ecfb9ee49b0d8a416383b2f885643c76b9
jeremias-gonzalez <rusitogonzalez482@gmail.com>
2024-10-27 11:32:29 -0300

    main

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const { google } = require('googleapis');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Lista de orígenes permitidos
 const allowedOrigins = [
